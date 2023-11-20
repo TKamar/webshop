08ca8c66b1f903f56b650b2b3632ffa1c4258f7a
Tomer <tomer.kamar@gmail.com>
2023-11-20 16:02:53 +0200

    Adding heder component and start working on the pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { from } from 'rxjs';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent
   ],
   imports: [
     BrowserModule,
